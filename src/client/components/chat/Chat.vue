86e821880ecadd1991dbf5e7240158717a39b722
Daniel García <d.garcia2@udc.es>
2024-04-05 12:29:55 +0200

    Finished chat functionality adding new messages as they are inputted and answered by the model

--- src/client/components/chat/Chat.vue
+++ src/client/components/chat/Chat.vue
@@ -1,11 +1,13 @@
 <script setup>
-  import { ref } from "vue";
+  import { ref, reactive } from "vue";
 
   const SERVER_URL = `${
     import.meta.env.VITE_SERVER_URL || "http://localhost:5000"
   }/api`;
-  const message = ref("");
-  let conversation = [];
+  let message = ref("");
+
+  const loadingResponse = ref(false);
+  let conversation = reactive([]);
   let nTokensConversation = 0;
 
   // Counts the approximate number of tokens in the input while adding the total number for the whole conversation
@@ -18,8 +20,22 @@
   }
 
   const getModelOutput = async () => {
+
+    if (loadingResponse.value) return
+
     try {
       const userMessage = message.value;
+
+      // Add the new message to the chat messages array
+      if (userMessage != "") {
+        conversation.push({
+          text: userMessage,
+          timestamp: new Date().toLocaleTimeString(),
+          isCurrentUser: true // You may modify this condition based on your logic
+        });
+        // Clear the input after sending the message
+        message.value = "";
+      }
 
       // Check that the amount of tokens does not exceed the maximum number of 4096 (as the counting is approximate we use 4000 instead)
       if (tokenCounter(userMessage) > 4000) {
@@ -27,6 +43,7 @@
         process.exit(1);
       } 
 
+      loadingResponse.value= true;
       // Send petition with the user input
       const response = await fetch(`${SERVER_URL}/chat`, {
         method: "POST",
@@ -40,20 +57,39 @@
       });
       if (!response.ok) {
         const error = await response.text();
+        loadingResponse.value= false;
         throw new Error(JSON.stringify(JSON.parse(error), null, 4));
       }
+      
 
       // Get response and update conversation and number of tokens
       const openAIData = await response.json();
-      conversation = openAIData.messagesHistory;
       nTokensConversation = openAIData.nTokens;
-      // TO DO: Change conversation
+      // Extract the last message from the server's response
+      const lastMessage = openAIData.messagesHistory.slice(-1)[0];
+      console.log(lastMessage);
+
+      // Add the last message to the conversation
+      if (lastMessage) {
+        conversation.push({
+          text: lastMessage.content,
+          timestamp: new Date().toLocaleTimeString(),
+          isCurrentUser: false // Set this to false to distinguish from user's messages
+        });
+      }
+
+      loadingResponse.value= false;
 
       console.log(openAIData);
     } catch (error) {
       throw new Error(error);
     }
   };
+
+
+  const copytoClipboard = (text) => {
+  // navigator.clipboard.
+  }
 </script>
 
 <template>
@@ -63,109 +99,24 @@
       class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden"
     >
       <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
-        <div class="flex w-full mt-2 space-x-3 max-w-xs">
-          <div
-            class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-200 items-center justify-center flex"
-          >
+        <!-- Render chat messages dynamically -->
+        <div v-for="(message, index) in conversation" :key="index" :class="{ 'ml-auto justify-end': message.isCurrentUser }" class="flex w-full mt-2 space-x-3 max-w-xs">
+          <div v-if="!message.isCurrentUser" class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-200 items-center justify-center flex">
             <!-- Image centered -->
             <img src="/codellamaicon.png" width="30" height="30" alt="" />
           </div>
-          <div>
-            <div class="bg-gray-300 p-3 rounded-e-lg rounded-bs-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+          <div :class="{ 'ml-0': !message.isCurrentUser, 'mr-0': message.isCurrentUser }">
+            <div :class="{ 'bg-blue-600 text-white align-end': message.isCurrentUser, 'bg-gray-300': !message.isCurrentUser }" class="p-3 rounded-lg">
+              <p class="text-sm">{{ message.text }}</p>
             </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+            <span @click="copytoClipboard(message.text)" class="text-xs text-gray-500 leading-none">{{ message.timestamp }}</span>
           </div>
+          <div v-if="message.isCurrentUser" class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
         </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod.
-              </p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs">
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-          <div>
-            <div class="bg-gray-300 p-3 rounded-e-lg rounded-bs-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt.
-              </p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs">
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-          <div>
-            <div class="bg-gray-300 p-3 rounded-e-lg rounded-bs-lg">
-              <p class="text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-        </div>
-        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-          <div>
-            <div class="bg-blue-600 text-white p-3 rounded-s-lg rounded-be-lg">
-              <p class="text-sm">Lorem ipsum dolor sit.</p>
-            </div>
-            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-          </div>
-          <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>
+      </div>
+
+      <div v-if="loadingResponse" class="flex flex-col items-center mb-3">
+        <v-progress-circular indeterminate></v-progress-circular>
       </div>
 
       <div class="bg-gray-300 p-4">
