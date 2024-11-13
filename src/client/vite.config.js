2ff8a896e5b352544b89a151467185433d43ec66
vlamas <vlamas@udc.es>
2024-11-13 10:56:17 +0200

    rollback production configs

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -6,7 +6,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
-  base: "/sensorbuilder",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, ""),
